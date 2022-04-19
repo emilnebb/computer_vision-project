f41336073ef6af89cbd64893122e000ef3b52350
emilnebb <emilneby@online.no>
2022-04-19 22:34:54 +0200

    Some resnet code

--- ssd/modeling/backbones/retina.py
+++ ssd/modeling/backbones/retina.py
@@ -9,8 +9,8 @@ import torchvision
 
 class RetinaNet(torch.nn.Module):
     """
-    This is a basic backbone for SSD.
-    The feature extractor outputs a list of 6 feature maps, with the sizes:
+    Backbone = FPN
+    FPN goes on top of ResNet
     """
     def __init__(self,
             output_channels: List[int],
@@ -43,33 +43,97 @@ class RetinaNet(torch.nn.Module):
             kernel_size=1,
             stride=1,
             padding=0
-        )        
+        )
+        nn.ConvTranspose2d
+        #FPN
+        self.fpn = torchvision.ops.FeaturePyramidNetwork([256]*5, 256)
+        self.x_fpn = OrderedDict()
+        #ResNet
+        self.resnet = torchvision.models.resnet18(pretrained=True, progress=True) #torchvision.models.resnet18(pretrained=True, progress=True)
+        #Change output dimension of last fully conencted layer in the ResNet
+        num_ftrs = self.resnet.fc.in_features
+        self.resnet.fc = nn.Linear(num_ftrs, 256)
+        #remove 2 last layers of resnet
 
-        self.fpn = torchvision.ops.FeaturePyramidNetwork([256] * 5, 256)
-        self.resnet = torchvision.models.wide_resnet50_2(pretrained=True, progress=True) #torchvision.models.resnet18(pretrained=True, progress=True)
+        #ONLY USE SELECTED LAYERS OF THE RESNET
+        self.resnet.conv1.stride = (4, 4)
+        print(self.resnet)
+        #self.resnet.conv1.in_channels = image_channels
+        #self.resnet.conv1.out_channels = 256
+
+        self.resnet.conv1.kernel_size = (1, 1)
+        #self.resnet.conv1.padding = (0,0)
+
+
 
     def forward(self, x):
         """
+        From paper:
+        RetinaNet uses feature pyramid levels P3 to P7, where P3 to P5 are computed
+        from the output of the corresponding ResNet residual stage (C3 through C5)
+        using top-down and lateral connections just as in [19], P6 is obtained via
+        a 3×3 stride-2 conv on C5, and P7 is computed by apply- ing ReLU followed
+        by a 3×3 stride-2 conv on P6. This differs slightly from [19]: (1) we don’t
+        use the high-resolution pyramid level P2 for com- putational reasons,
+        (2) P6 is computed by strided convolution instead of downsampling,
+        and (3) we include P7 to improve large object detection.
+        These minor modifications improve speed while maintaining accuracy.
+        """
+        print(x.shape)
+        self.resnet.conv1.in_channels = 64
+        self.resnet.eval()
+        #x = self.resnet(x)
+        #print(x.shape)
+        print("Conv 1:")
+
+        self.resnet.conv1.in_channels = 3
+        self.resnet.conv1.out_channels = 256
+        #self.resnet.conv1.padding = (0, 0)
+        self.resnet.conv1.kernel_size = (1, 1)
+        self.resnet.conv1.stride = (2, 2)
+        y = self.resnet.conv1(x)
+        print(y.shape)
+
+        print("Layer 1:")
+        y = self.resnet.layer1(y) #keeps the same shape
+        print(y.shape)
+        print("Layer 1:") # keeps the same shape
+        print(self.resnet.layer1.children())
+        print(y.shape)
+        print("Layer 2:")
+        y = self.resnet.layer2(y)
+        print(y.shape)
+        print("Layer 3:")
+        y = self.resnet.layer3(y)
+        print(y.shape)
+        print("Layer 3:")
+        y = self.resnet.layer3[1](y)
+        print(y.shape)
 
         """
+        self.x_fpn['P_2'] = self.transform_stride_4(x) # torch.Size([1, 256, 32, 256])
+        self.x_fpn['P_3'] = self.transform_stride_2(self.x_fpn['P_2']) # torch.Size([1, 256, 16, 128])
+        self.x_fpn['P_4'] = self.transform_stride_2(self.x_fpn['P_3']) # torch.Size([1, 256, 8, 64])
+        self.x_fpn['P_5'] = self.transform_stride_2(self.x_fpn['P_4']) # torch.Size([1, 256, 4, 32])
+        self.x_fpn['P_6'] = self.transform_stride_2(self.x_fpn['P_5']) # torch.Size([1, 256, 2, 16])
+        self.x_fpn['P_7'] = self.transform_stride_2(self.x_fpn['P_6']) # torch.Size([1, 256, 1, 8])
+        """
 
-        x_fpn = OrderedDict()
-        x_fpn['P_2'] = self.transform_stride_4(x) # torch.Size([1, 256, 32, 256])
-        x_fpn['P_3'] = self.transform_stride_2(x_fpn['P_2']) # torch.Size([1, 256, 16, 128])
-        x_fpn['P_4'] = self.transform_stride_2(x_fpn['P_3']) # torch.Size([1, 256, 8, 64])
-        x_fpn['P_5'] = self.transform_stride_2(x_fpn['P_4']) # torch.Size([1, 256, 4, 32])
-        x_fpn['P_6'] = self.transform_stride_2(x_fpn['P_5']) # torch.Size([1, 256, 2, 16])
-        x_fpn['P_7'] = self.transform_stride_2(x_fpn['P_6']) # torch.Size([1, 256, 1, 8])
+        self.x_fpn['P_2'] = self.transform_stride_4(x) # torch.Size([1, 256, 32, 256])
+        self.x_fpn['P_3'] = self.transform_stride_2(self.x_fpn['P_2']) # torch.Size([1, 256, 16, 128])
+        self.x_fpn['P_4'] = self.transform_stride_2(self.x_fpn['P_3']) # torch.Size([1, 256, 8, 64])
+        self.x_fpn['P_5'] = self.transform_stride_2(self.x_fpn['P_4']) # torch.Size([1, 256, 4, 32])
+        self.x_fpn['P_6'] = self.transform_stride_2(self.x_fpn['P_5']) # torch.Size([1, 256, 2, 16])
+        self.x_fpn['P_7'] = self.transform_stride_2(self.x_fpn['P_6']) # torch.Size([1, 256, 1, 8])
 
-        outputs = self.fpn(x_fpn).values()
+        outputs = self.fpn(self.x_fpn).values()
 
         #print(outputs_fpn.values().next().shape)
-        outputs = [self.transform_out_channels_3(output) for output in outputs]
+        #outputs = [self.transform_out_channels_3(output) for output in outputs]
 
         # set model in evaluation mode
-        self.resnet.eval()
-
-        outputs = [self.resnet(output) for output in outputs]
+        #self.resnet.eval()
+        #outputs = [self.resnet(output) for output in outputs]
 
         for idx, feature in enumerate(outputs):
             out_channel = self.out_channels[idx]
