6dba3763c8407e53156719d46d463fd91a4f5f7a
emilnebb <emilneby@online.no>
2022-05-03 10:58:18 +0200

    Added more plots and logs for tensorboard

--- tensorboard/plot.py
+++ tensorboard/plot.py
@@ -26,22 +26,17 @@ if __name__ == '__main__':
     augmentation = load_loss(path="data/run-task_2.2_augmentation-tag-loss_total_loss.csv")
     retina = load_loss(path="data/run-task_2.3.1_retinanet-tag-loss_total_loss.csv")
     deeper_heads = load_loss(path="data/run-task_2.3.3_deeper_heads-tag-loss_total_loss.csv")
+    weight_init = load_loss(path="data/run-task_2.3.4_weight_init-tag-loss_total_loss.csv")
+    focal_loss = load_loss(path="data/run-task_2.3.5_focal_loss-tag-loss_total_loss.csv")
 
-    """
-    weight_init = load_loss(path=" ")
-    focal_loss = load_loss(path=" ")
-    """
+    plt.figure(figsize=(12, 8))
+    plt.plot(step, basic, label='Basic model', color='red')
+    plt.plot(step, augmentation, label='Data augmentation', color='magenta')
+    plt.plot(step, retina, label='FPN on ResNet', color='cyan')
+    plt.plot(step, deeper_heads, label='Deeper convolutional heads', color='orange')
+    plt.plot(step, weight_init, label='Improved weight and bias initialization', color='green')
+    plt.plot(step, focal_loss, label='Focal loss', color='blue')
 
-
-    plt.plot(step, basic, label='Basic model')
-    plt.plot(step, augmentation, label='Data augmentation')
-    plt.plot(step, retina, label='FPN on ResNet')
-    plt.plot(step, deeper_heads, label='Deeper convolutional heads')
-
-    """
-    plt.plot(step, weight_init, label='Improved weight and bias initialization')
-    plt.plot(step, focal_loss, label='Focal loss')
-    """
 
     plt.xlabel('Train step')
     plt.ylabel('Total loss')
