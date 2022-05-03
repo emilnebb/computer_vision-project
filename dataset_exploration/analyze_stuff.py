88b0da0f82eacd843b194ab5401892cd55e9579d
Elias Wendt <git@eliaswendt.de>
2022-05-03 12:36:57 +0200

    Update analysis

--- dataset_exploration/analyze_stuff.py
+++ dataset_exploration/analyze_stuff.py
@@ -27,17 +27,21 @@ def analyze_something(dataloader, cfg):
     rel_bb_sizes = []
     aspect_ratios = []
 
-    total_class_occurrences = {
-        'background': 0,
-        'car': 0,
-        'truck': 0,
-        'bus': 0,
-        'motorcycle': 0,
-        'bicycle': 0,
-        'scooter': 0,
-        'person': 0,
-        'rider': 0,
+    bb_dimensions = []
+
+    # counting # occurrences and sum of bb sizes
+    total_class_counters = {
+        'background': (0, 0.),
+        'car': (0, 0.),
+        'truck': (0, 0.),
+        'bus': (0, 0.),
+        'motorcycle': (0, 0.),
+        'bicycle': (0, 0.),
+        'scooter': (0, 0.),
+        'person': (0, 0.),
+        'rider': (0, 0.),
     }
+
 
     for i, batch in tqdm(enumerate(dataloader)):
 
@@ -68,29 +72,39 @@ def analyze_something(dataloader, cfg):
             # translate label index into label string
             label = cfg.label_map[label.item()]
 
-            total_class_occurrences[label] += 1
-
             rel_x1, rel_x2 = batch['boxes'][0][idx][0].item(), batch['boxes'][0][idx][2].item()
             rel_y1, rel_y2 = batch['boxes'][0][idx][1].item(), batch['boxes'][0][idx][3].item()
-            
+
             # as coordinates are given relatively to dimensions this
             # calculates the relative size of bb compared to entire image
             rel_width = rel_x2 - rel_x1
             rel_height = rel_y2 - rel_y1
 
+            abs_width = rel_width * batch['width'].item()
+            abs_height = rel_height * batch['height'].item()
+
+            bb_dimensions.append((abs_width, abs_height, label))
+
+            current = total_class_counters[label]
+            total_class_counters[label] = (current[0] + 1, current[1] + rel_width * rel_height)
+
+            # abs_width = rel_width * batch['width']
 
 
             rel_bb_size[label] += rel_width * rel_height
             rel_bb_size['sum'] += rel_width * rel_height
             
             # we use scalar as aspect ratio (height/width)
+            # because most of the bb boxes have greater hight than width
             aspect_ratios.append(rel_height / rel_width)
 
         rel_bb_sizes.append(rel_bb_size)
 
-    with open('dataset_exploration/total_class_occurrences.json', 'w') as f:
-        json.dump(total_class_occurrences, f)
-    
+    with open('dataset_exploration/bb_dimensions.json', 'w') as f:
+        json.dump(bb_dimensions, f)
+        
+    with open('dataset_exploration/total_class_counters.json', 'w') as f:
+        json.dump(total_class_counters, f)
 
     with open('dataset_exploration/bb_sizes.json', 'w') as f:
         json.dump(rel_bb_sizes, f)
