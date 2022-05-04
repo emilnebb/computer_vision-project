0d84be4714d36c8a4b1f5418f574ea93a18f924d
Elias Wendt <git@eliaswendt.de>
2022-05-04 16:12:25 +0200

    Update aspect_ratios

--- configs/tdt4265_optimized_anchor_boxes.py
+++ configs/tdt4265_optimized_anchor_boxes.py
@@ -25,7 +25,7 @@ anchors = L(AnchorBoxes)(
     # aspect ratio is used to define two boxes per element in the list.
     # if ratio=[2], boxes will be created with ratio 1:2 and 2:1
     # Number of boxes per location is in total 2 + 2 per aspect ratio
-    aspect_ratios=[[2, 3, 5, 11], [2, 3, 5, 11], [2, 3, 5, 11], [2, 3, 5, 11], [2, 3, 5, 11], [2, 3, 5, 11]],
+    aspect_ratios=[[1.5, 2, 3, 4], [1.5, 2, 3, 4], [1.5, 2], [1.5, 2], [2], [2]],
     image_shape="${train.imshape}",
     scale_center_variance=0.1,
     scale_size_variance=0.2
