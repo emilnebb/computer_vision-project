c1d111416d7eac8e2b07cf707e99fd6a90400232
Elias Wendt <git@eliaswendt.de>
2022-05-06 19:20:49 +0200

    Add train configuration

--- detectron_experiments/train.sh
+++ detectron_experiments/train.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+export CUDA_VISIBLE_DEVICES=10 # just set GPU:10 visible; torch can't handle more than 16 GPUs by default
 
-python tools/plain_train_net.py \
---config-file configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml \
+python trainer.py \
+--config-file config.yaml \
+--num-gpus 1 
+#SOLVER.IMS_PER_BATCH 2 SOLVER.BASE_LR 0.0025 \
 #--opts MODEL.DEVICE cpu \
 #--webcam \
 #--input "../datasets/tdt4265/dataset/images/val/*" \
