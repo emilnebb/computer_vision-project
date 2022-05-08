5650a35f899e78d919be1b12d91ff3f00e98d987
emilnebb <emilneby@online.no>
2022-05-08 10:59:31 +0200

    Grad cam, 1st iteration works. Still need some more testing and modification

--- grad_cam/cam.py
+++ grad_cam/cam.py
@@ -1,38 +1,24 @@
-import PIL.Image
 import cv2
 import os
 import tops
 import click
 import numpy as np
-import matplotlib.pyplot as plt
 from collections import OrderedDict
-from tops.config import instantiate
-from tops.config import LazyCall as L
-from tops.checkpointer import load_checkpoint
-#from vizer.draw import draw_boxes
-from ssd import utils
-from tqdm import tqdm
-from ssd.data.transforms import ToTensor
-import torchvision
 
-from performance_assessment.save_comparison_images import get_config, get_trained_model, get_dataloader, convert_boxes_coords_to_pixel_coords, convert_image_to_hwc_byte, create_filepath, create_comparison_image, create_and_save_comparison_images
+
+from performance_assessment.save_comparison_images import get_config, get_trained_model, get_dataloader
 
 import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
-from torchvision.models.segmentation import deeplabv3_resnet50
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
-import requests
 from PIL import Image
-from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam import AblationCAM, EigenCAM
-from pytorch_grad_cam.ablation_layer import AblationLayerFasterRCNN
+from pytorch_grad_cam import EigenCAM
 from pytorch_grad_cam.utils.model_targets import FasterRCNNBoxScoreTarget
 from pytorch_grad_cam.utils.reshape_transforms import fasterrcnn_reshape_transform
-from pytorch_grad_cam.utils.image import show_cam_on_image, scale_accross_batch_and_channels, scale_cam_image, preprocess_image
-
+from pytorch_grad_cam.utils.image import show_cam_on_image
 
 #Piazza thread for grad grad_cam:
 #https://piazza.com/class/kyipdksfp9q1dn?cid=420
@@ -59,7 +45,6 @@ class RetinaNetModelOutputWrapper(torch.nn.Module):
 
 def fasterrcnn_reshape_transform(x):
     target_size = x[3].shape[-2 : ]
-    #print(f"Target size {target_size}")
     activations = []
     for value in x:
         activations.append(torch.nn.functional.interpolate(torch.abs(value), target_size, mode='bilinear'))
@@ -121,48 +106,12 @@ def main(config_path, train, num_images, conf_threshold):
 
     dataloader = get_dataloader(cfg, dataset_to_visualize)
     img_tensor = next(iter(dataloader))['image'] #tensor
-    #print(f"Image tensor shape {img_tensor.shape}")
 
     img_numpy = img_tensor[0].numpy().transpose(1, 2, 0)
     img_tensor = tops.to_cuda(Variable(img_tensor, requires_grad=True))
-    
-    #plt.imshow(img_numpy)
-    #plt.show()
-    image_float_np = np.float32(img_numpy) / 255
-
-    """
-    boxes, labels, scores = model(img_tensor)[0]
-    print(f"Boxes type {type(boxes)}")
-    print(f"Labels type {type(labels)}")
-    print(f"Scores type {type(scores)}")
-    boxes = Variable(boxes, requires_grad=True)
-    """
-
 
     wrapped_model = RetinaNetModelOutputWrapper(model)
     boxes, labels, indices = predict(img_tensor, wrapped_model)
-
-    """
-    image = np.array(PIL.Image.open("grad_cam/both.png"))
-    image_float_np = np.float32(image) / 255
-    # define the torchvision image transforms
-    transform = torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor(),
-    ])
-
-    input_tensor = transform(image)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    input_tensor = input_tensor.to(device)
-    # Add a batch dimension:
-    input_tensor = input_tensor.unsqueeze(0)
-
-    test_model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-    test_model.eval()
-    boxes, classes, labels, indices = predict(img_tensor, model, device, 0.9)
-    print(type(boxes))
-    
-    test_target_layers = [test_model.backbone]
-    """
 
     target_layers = [model.feature_extractor]
     targets = [FasterRCNNBoxScoreTarget(labels=labels, bounding_boxes=boxes)]
@@ -170,15 +119,20 @@ def main(config_path, train, num_images, conf_threshold):
                    target_layers,
                    use_cuda=torch.cuda.is_available(),
                    reshape_transform=fasterrcnn_reshape_transform)
+
+    #Turn off gradient calculations
     cam.uses_gradients = False
 
     grayscale_cam = cam(img_tensor, targets=targets)
     # Take the first image in the batch:
     grayscale_cam = grayscale_cam[0, :]
-    cam_image = show_cam_on_image(image_float_np, grayscale_cam, use_rgb=True)
+    cam_image = show_cam_on_image(img_numpy, grayscale_cam, use_rgb=True)
     # And lets draw the boxes again:
     image_with_bounding_boxes = draw_boxes(boxes, labels, cam_image)
-    Image.fromarray(image_with_bounding_boxes)
+    #print(f"Image type: {type(image_with_bounding_boxes)}")
+    img = Image.fromarray(image_with_bounding_boxes)
+    img.save("grad_cam/grad_cam_pictures/grad.png")
+    img.show()
 
 
 
