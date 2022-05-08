b0b83d09c7718678ff76a6bf0235ee0038b440ea
emilnebb <emilneby@online.no>
2022-05-08 22:37:34 +0200

    Last changes implemented

--- grad_cam/cam.py
+++ grad_cam/cam.py
@@ -1,3 +1,4 @@
+import PIL.Image
 import cv2
 import os
 import tops
@@ -7,7 +8,7 @@ from collections import OrderedDict
 
 
 from performance_assessment.save_comparison_images import get_config, get_trained_model, get_dataloader
-
+import torchvision
 import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
@@ -45,9 +46,11 @@ class RetinaNetModelOutputWrapper(torch.nn.Module):
         dict_from_outputs["scores"] = s
         return [dict_from_outputs]
 
+coco_names = ['__background__', 'car', 'truck', 'bus', 'motorcycle', 'bicycle', 'scooter', 'person']
 
 def fasterrcnn_reshape_transform(x):
-    target_size = x[3].shape[-2 : ]
+    #Pick first layer in feature map to get more fine grained visualization
+    target_size = x[0].shape[-2 : ]
     activations = []
     for value in x:
         activations.append(torch.nn.functional.interpolate(torch.abs(value), target_size, mode='bilinear'))
@@ -56,21 +59,23 @@ def fasterrcnn_reshape_transform(x):
 
 def predict(input_tensor, model):
     outputs = model(input_tensor)
+    pred_classes = [coco_names[i] for i in outputs[0]['labels'].cpu().numpy()]
     pred_labels = outputs[0]['labels'].cpu().numpy()
     pred_scores = outputs[0]['scores'].detach().cpu().numpy()
     pred_bboxes = outputs[0]['boxes'].detach().cpu().numpy()
 
-    boxes, labels, indices = [], [], []
+    boxes, classes, labels, indices = [], [], [], []
     for index in range(len(pred_scores)):
         boxes.append(pred_bboxes[index].astype(np.int32))
+        classes.append(pred_classes[index])
         labels.append(pred_labels[index])
         indices.append(index)
     boxes = np.int32(boxes)
-    return boxes, labels, indices
+    return boxes, classes, labels, indices
 
-COLORS = np.random.uniform(0, 255, size=(9, 3))
+COLORS = np.random.uniform(0, 255, size=(len(coco_names), 3))
 
-def draw_boxes(boxes, labels, image):
+def draw_boxes(boxes, labels, classes, image):
     for i, box in enumerate(boxes):
         color = COLORS[labels[i]]
         cv2.rectangle(
@@ -79,7 +84,7 @@ def draw_boxes(boxes, labels, image):
             (int(box[2]), int(box[3])),
             color, 2
         )
-        cv2.putText(image, (int(box[0]), int(box[1] - 5)),
+        cv2.putText(image, classes[i], (int(box[0]), int(box[1] - 5)),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2,
                     lineType=cv2.LINE_AA)
     return image
@@ -108,13 +113,17 @@ def main(config_path, train, num_images, conf_threshold):
         dataset_to_visualize = "val"
 
     dataloader = get_dataloader(cfg, dataset_to_visualize)
-    img_tensor = next(iter(dataloader))['image'] #tensor
+    dataloader = iter(dataloader)
+
+    #Iterate into the datset and pick a picture
+    for i in range(0,150):
+        img_tensor = next(dataloader)['image'] #tensor
 
     img_numpy = img_tensor[0].numpy().transpose(1, 2, 0)
     img_tensor = tops.to_cuda(Variable(img_tensor, requires_grad=True))
 
     wrapped_model = RetinaNetModelOutputWrapper(model)
-    boxes, labels, indices = predict(img_tensor, wrapped_model)
+    boxes, classes, labels, indices = predict(img_tensor, wrapped_model)
 
     target_layers = [model.feature_extractor]
     targets = [FasterRCNNBoxScoreTarget(labels=labels, bounding_boxes=boxes)]
@@ -131,10 +140,10 @@ def main(config_path, train, num_images, conf_threshold):
     grayscale_cam = grayscale_cam[0, :]
     cam_image = show_cam_on_image(img_numpy, grayscale_cam, use_rgb=True)
     # And lets draw the boxes again:
-    image_with_bounding_boxes = draw_boxes(boxes, labels, cam_image)
+    image_with_bounding_boxes = draw_boxes(boxes, labels, classes, cam_image)
     #print(f"Image type: {type(image_with_bounding_boxes)}")
     img = Image.fromarray(image_with_bounding_boxes)
-    img.save("grad_cam/grad_cam_pictures/grad.png")
+    img.save("grad_cam/grad_cam_pictures/grad150_0.png")
     img.show()
 
 
